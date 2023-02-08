4ed467b72b955cc3cbb0ebb94175444bbb8c0e86
dennisnderitu254 <dknderitu@gmail.com>
2023-02-08 16:05:08 +0300

    updating filestorage and console with new classes for serialization and deserialization

--- console.py
+++ console.py
@@ -3,6 +3,11 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):

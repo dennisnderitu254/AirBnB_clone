4176f28c2a3731c3f7ce1a247516f3b15638ca5e
dennisnderitu254 <dknderitu@gmail.com>
2023-02-08 16:13:43 +0300

    testing parse function

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the HBNB console"""
 import cmd
+# import re
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,6 +9,24 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+
+
+# def parse(arg):
+#     curly_braces = re.search(r"\{(.*?)\}", arg)
+#     brackets = re.search(r"\[(.*?)\]", arg)
+#     if curly_braces is None:
+#         if brackets is None:
+#             return [i.strip(",") for i in split(arg)]
+#         else:
+#             lexer = split(arg[:brackets.span()[0]])
+#             retl = [i.strip(",") for i in lexer]
+#             retl.append(brackets.group())
+#             return retl
+#     else:
+#         lexer = split(arg[:curly_braces.span()[0]])
+#         retl = [i.strip(",") for i in lexer]
+#         retl.append(curly_braces.group())
+#         return retl
 
 
 class HBNBCommand(cmd.Cmd):

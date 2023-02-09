ad48d1ab568a74febc4ca3fef6c9ca2b57c0019e
dennisnderitu254 <dknderitu@gmail.com>
2023-02-09 12:38:07 +0300

    model test files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -7,6 +6,8 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+import os.path
+import json
 
 
 class FileStorage:
